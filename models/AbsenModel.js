8a66ff64752402cb9ad115c86ee3bc8f3c61dab4
synysterR3v <saepuladi4@gmail.com>
2023-12-08 17:18:32 +0700

    incorectly formed perpus

---
+++ models/AbsenModel.js
@@ -0,0 +1,55 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import db from '../config/Database.js';
+import Users from './UserModel.js';
+import Perpustakaan from './PerpusModel.js';
+
+const Absen = db.define('absens', {
+  absen_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tanggal_absensi: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  waktu_masuk: {
+    type: DataTypes.TIME,
+    allowNull: false,
+  },
+  waktu_keluar: {
+    type: DataTypes.TIME,
+    allowNull: true,
+  },
+  durasi_kehadiran: {
+    type: DataTypes.INTEGER, 
+    allowNull: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Users,
+      key: "user_id",
+    }
+  },
+  perpus_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Perpustakaan,
+      key: "perpustakaan_id", // Perhatikan penyesuaian key disini
+    }
+  },
+}, {
+  freezeTableName: true,
+  timestamps: false,
+});
+
+Users.hasMany(Absen, { foreignKey: 'user_id' });
+Absen.belongsTo(Users, { foreignKey: 'user_id' });
+
+Perpustakaan.hasMany(Absen, { foreignKey: 'perpus_id' });
+Absen.belongsTo(Perpustakaan, { foreignKey: 'perpus_id' });
+
+export default Absen;
