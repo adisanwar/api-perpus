83569a6d4c88fff962849bcb7b1a6e249d744960
synysterRev <69097348+synysterR3v@users.noreply.github.com>
2024-01-01 12:54:15 +0700

    modified config

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,16 @@
 import { Sequelize } from "sequelize";
-
+import mysql2 from 'mysql2';
 
 let db;
+
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
 
 try {
   db = new Sequelize('perpus', 'perpus', 'Admin@)!@', {
     host: "8.219.70.58",
-    dialect: "mysql"
+    dialect: "mysql2"
   });
 
   // Additional configurations or initialization can be done here if needed
