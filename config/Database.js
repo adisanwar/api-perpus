524a7e9d086bdb5b850ed80e1cad4fe8a0238e8e
synysterRev <69097348+synysterR3v@users.noreply.github.com>
2024-01-01 13:03:40 +0700

    modified config

--- config/Database.js
+++ config/Database.js
@@ -1,33 +1,26 @@
 import { Sequelize } from "sequelize";
-import mysql2 from 'mysql2';
+import mysql2 from "mysql2";
 
 let db;
 
-if (options.dialect === 'mysql') {
-  options.dialectModule = mysql2;
-}
-
 try {
-  db = new Sequelize('perpus', 'perpus', 'Admin@)!@', {
+  db = new Sequelize("perpus", "perpus", "Admin@)!@", {
     host: "8.219.70.58",
-    dialect: "mysql2"
+    dialect: "mysql",
   });
-
-  // Additional configurations or initialization can be done here if needed
-
-  // Testing the connection (optional)
+ 
   db.authenticate()
     .then(() => {
-      console.log('Connection has been established successfully.');
+      console.log("Connection has been established successfully.");
     })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
     });
 } catch (error) {
-  console.error('Error occurred during database initialization:', error);
+  console.error("Error occurred during database initialization:", error);
   // Handle or log the error accordingly
 }
 
 // Use the 'db' variable for further database operations
 
-export default db; 
+export default db;
